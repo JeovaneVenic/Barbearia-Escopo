2c10e14fb661d64feb142e47f51a4916793aed39
jeovane venancio <jeovanebs@outlook.com>
2024-10-11 12:38:22 -0300

    Subindo todos os arquivos

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="container flex h-[80vh] w-[90%] items-center justify-center rounded-lg">
+      <h2 className="p-2 text-3xl text-white">Inicio project Barbearia</h2>
+    </section>
+  )
+}
